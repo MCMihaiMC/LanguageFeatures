711cc3dc75b0f17fd1284cdbdeaf275275eeb544
mcmihaimc <muntean.mihai70@gmail.com>
2022-02-21 18:13:11 +0200

    Using the null conditional operator

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            if (Product.GetProducts().Length > 0)
+            {
+                foreach (Product p in Product.GetProducts())
+                {
+                    string name = p?.Name;
+                    decimal? price = p?.Price;
+
+                    results.Add(string.Format($"Name: {name}, Price: {price}"));
+                }
+
+                return View(results);
+            }
+            else 
+                return View(new string[] { "C#", " Language", "Features" });
+        }
+    }
+}
